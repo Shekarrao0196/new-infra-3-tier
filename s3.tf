1f93b9909f170d6cfd9a4a19cf7d4894e6a7462c
Shekarrao0196 <shekarraok612@gmail.com>
2025-04-01 13:50:59 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,7 @@
-# Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "swiggyshekarbucket557204"
+  bucket = "example-bucket"
 
-  # Enable versioning
   versioning {
     enabled = true
   }
 }
-
