621641bb8397c8cfd593dcbb6cb1a1735461f09c
Shekarrao0196 <shekarraok612@gmail.com>
2025-04-01 14:03:55 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,10 @@
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "example-bucket"
+  bucket = "swiggy-buckefuvghvhgrdrdhdhfdc11"
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "example_versioning" {
+  bucket = aws_s3_bucket.example_bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
